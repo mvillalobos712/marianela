12efca3ba681405fe6ee0fa8e39614e1d57ad1e6
mvillalobos712 <mvillalobos712@gmail.com>
2017-05-24 15:19:09 -0400

    Add files via upload

---
+++ url.py
@@ -0,0 +1,7 @@
+<HTML><HEAD>
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+<TITLE>Redirecting</TITLE>
+<META HTTP-EQUIV="refresh" content="1; url=https://github.com/mvillalobos712/python2/blob/master/ej1.py">
+</HEAD>
+<BODY onLoad="location.replace('https://github.com/mvillalobos712/python2/blob/master/ej1.py'+document.location.hash)">
+Redirecting you to https://github.com/mvillalobos712/python2/blob/master/ej1.py</BODY></HTML>
